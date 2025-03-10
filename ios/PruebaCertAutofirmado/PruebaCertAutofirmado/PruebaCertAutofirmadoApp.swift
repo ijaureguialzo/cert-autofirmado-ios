35c2d1673f7fbb5a205a6139561082120b4b4a38
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2025-03-11 00:29:36 +0100

    Proyecto nuevo

---
+++ ios/PruebaCertAutofirmado/PruebaCertAutofirmado/PruebaCertAutofirmadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaCertAutofirmadoApp.swift
+//  PruebaCertAutofirmado
+//
+//  Created by Ion Jaureguialzo Sarasola on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PruebaCertAutofirmadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
